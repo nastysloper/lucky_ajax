374d24a9e8e1a6a4cda4f7c4aaa300ff4cc264c1
Richard Vogt <richvogtjr@gmail.com>
2013-07-20 13:09:17 -0500

    adding comments

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,25 +1,24 @@
 get '/' do
-  @roll = Roll.create
+  @roll = Roll.create # I added this line so the die appears upon initial page load.
   erb :index
 end
 
-
-# TODO: convert this route to use AJAX
 post '/rolls' do
 
   # If the user passes-in a "value", let's use it. Otherwise, we'll generate a random one.
   # See: roll_if_value_is_nil method in the Roll model.
-  #value = params[:value] ? params[:value].to_i : nil
+  
+  value = params[:value] ? params[:value].to_i : nil
 
-  value = params[:value]
+  #value = params[:value]
 
-  #@roll = value ? Roll.create({ value: value }) : Roll.create
-  @roll = Roll.create({ value: value })
+  @roll = value ? Roll.create({ value: value }) : Roll.create
+  #@roll = Roll.create({ value: value })
 
-  if request.xhr?
-    erb :roll, :layout => false
-  else
+  #if request.xhr?
+  #  erb :roll, :layout => false
+  #else
     erb :index  # HINT: what does this do? what should we do instead?
-  end
+  #end
 
 end

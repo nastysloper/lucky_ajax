97dbe440eb9977ffe52766e4e59e5e7b8f751ae7
Richard Vogt <richvogtjr@gmail.com>
2013-07-20 12:46:23 -0500

    add die image to initial landing page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,15 +1,24 @@
 get '/' do
+  @roll = Roll.create
   erb :index
 end
 
 
 # TODO: convert this route to use AJAX
 post '/rolls' do
+
   # If the user passes-in a "value", let's use it. Otherwise, we'll generate a random one.
   # See: roll_if_value_is_nil method in the Roll model.
-  value = params[:value] ? params[:value].to_i : nil
+  #value = params[:value] ? params[:value].to_i : nil
 
-  @roll = value ? Roll.create({ value: value }) : Roll.create
+  value = params[:value]
 
-  erb :index  # HINT: what does this do? what should we do instead?
+  #@roll = value ? Roll.create({ value: value }) : Roll.create
+  @roll = Roll.create({ value: value })
+
+  if request.xhr?
+    erb :roll, :layout => false
+  else
+    erb :index  # HINT: what does this do? what should we do instead?
+  end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,9 +1,20 @@
+var onSuccessHandler = function(responseContent) {
+  $("#die").html(responseContent);
+};
+
 $(document).ready(function () {
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+    var action = $(this).attr("action");
+    var formData = $(this).serialize();
+
+    $.post(action, formData, onSuccessHandler);
+  });
 
   // PSEUDO-CODE:
-  //   1- intercept the form submission event using jQuery
-  //   2- prevent the default action for that event from happening
-  //   3- generate a random number between 1 and 6 using JavaScript
+  //   1- intercept the form submission event using jQuery (line 6)
+  //   2- prevent the default action for that event from happening (line 7)
+  //   3- generate a random number between 1 and 6 using JavaScript (line )
   //   4- use jQuery to submit an AJAX post to the form's action
   //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
 

7a5be9339fa094d9aae8cd08d473f19237ac324c
Richard Vogt <richvogtjr@gmail.com>
2013-08-04 10:59:14 -0500

    add some more ajax

--- public/js/application.js
+++ public/js/application.js
@@ -2,21 +2,33 @@ var onSuccessHandler = function(responseContent) {
   $("#die").html(responseContent);
 };
 
+
+var onJaxHandler = function(responseContent) {
+  $('#jax').append(responseContent);
+};
+
 $(document).ready(function () {
   $("form").on("submit", function(event) {
     event.preventDefault();
     var action = $(this).attr("action");
     var formData = $(this).serialize();
 
-    $.post(action, formData, onSuccessHandler);
+    $.post('action', formData, onSuccessHandler);
   });
 
-  // PSEUDO-CODE:
-  //   1- intercept the form submission event using jQuery (line 6)
-  //   2- prevent the default action for that event from happening (line 7)
-  //   3- generate a random number between 1 and 6 using JavaScript (line )
-  //   4- use jQuery to submit an AJAX post to the form's action
-  //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
+
+  $('#jaxForm').on('submit', function(event){
+    event.preventDefault();
+
+    var action = $(this).attr('action'); // this is the url
+    var formData = $(this).serialize(); // this is the data in the form
+    
+    console.log(formData);  // I'm getting jaxster=97 
+
+    $.post('action', formData, onJaxHandler);
+  });
+});
+
 
 // $('.container').on("submit", '#rollForm', function(e){
 //   e.preventDefault()
@@ -24,6 +36,3 @@ $(document).ready(function () {
 //     $('#die').html(data)
 //   })
 // })
-
-
-});
